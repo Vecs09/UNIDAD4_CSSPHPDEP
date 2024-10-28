f9716e7be32052306bf67a8d5a6c9aded0e92bc5
Vecs <166658809+Vecs09@users.noreply.github.com>
2024-10-28 14:28:24 -0700

    Mostrar los articulos

--- t2/app/AuthController.php
+++ t2/app/AuthController.php
@@ -1,39 +1,49 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['usuario'];
-    $password = $_POST['contrasena'];
+session_start(); // Inicia la sesión
 
-    $curl = curl_init();
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'login') {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => '',
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => 'POST',
-      CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
-    ));
-    
-    $response = curl_exec($curl);
-    
-    curl_close($curl);
-    echo $response;
-
-    
-
-    $result = json_decode($response, true);
-
-    if ($result['code'] === 2) {
-        $_SESSION['data']= $result;
-        header('Location: vista.html'); 
-    } else {
-        echo '<script>alert("Usuario o contraseña incorrectos. Intente de nuevo.");</script>';
-        echo '<script>window.history.back();</script>'; 
-    }
-} else {
-    header('Location: vista.html'); 
-    exit();
+    $controller = new Controller();
+    $controller->access($email, $password);
 }
+
+class Controller {
+    public function access($email, $password) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('email' => $email, 'password' => $password),
+        ));
+
+        $response = curl_exec($curl);
+
+        if (curl_errno($curl)) {
+            echo 'Error en CURL: ' . curl_error($curl);
+            curl_close($curl);
+            return;
+        }
+
+        curl_close($curl);
+        $response = json_decode($response);
+
+        if (isset($response->data) && is_object($response->data)) {
+            $_SESSION['user_data'] = $response->data;
+            header("Location: /GitHub/UNIDAD4_CSSPHPDEP/t2/vista.html");
+            exit();
+        } else {
+            header("Location: login.html");
+            exit();
+        }
+    }
+}
+?>
